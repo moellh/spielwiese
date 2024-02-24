44f1679b6f58fd59a7abb23904c208440a4335e1
Henrik Möllmann <henmoell@gmail.com>
2024-02-24 13:56:18 +0100

    Added sets_over_k

---
+++ nolib/sets.py
@@ -0,0 +1,28 @@
+
+
+def sets_over_k(n,k,start=0):
+    '''
+    Returns all sets of length k with elements from 1 to n, starting from start.
+
+    Parameters
+    ----------
+    n : int:
+
+    k : int
+
+    start : int = 0
+
+    Returns
+    -------
+    list: list of sets of length k with elements from 1 to n, starting from start
+    '''
+
+    elements = []
+    if k==1:
+        for j in range(start,n+1):
+            elements.append({j})
+    else:
+        for i in range(start,n-k+2): # start to n-k+1 (last element to allow set of length k)
+            for s in sets_over_k(n,k-1,i+1):
+                elements.append({i}.union(s))
+    return elements
---
+++ tests/test_sets.py
@@ -0,0 +1,8 @@
+from nolib import sets
+
+def test_sets_over_k():
+    expected = [
+        {2, 3, 4}, {2, 3, 5}, {2, 3, 6}, {2, 4, 5}, {2, 4, 6}, {2, 5, 6}, {3, 4, 5}, {3, 4, 6}, {3, 5, 6}, {4, 5, 6}
+    ]
+    actual = sets.sets_over_k(6, 3, 2)
+    assert expected == actual
